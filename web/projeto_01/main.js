54913a77b76b1d5c319c53063011602b4fefd0f6
cmlfntss <camiladejesusfontes@gmail.com>
2023-07-19 21:04:25 -0300

    subindo arquivos iniciais dos projetos 01,02,03

---
+++ web/projeto_01/main.js
@@ -0,0 +1,2 @@
+
+console.log('Meu nome é Camila!') /* método do JS que quando chamado retorna o que está dentro do parênteses para o console */
---
+++ web/projeto_02/main.js
@@ -0,0 +1,5 @@
+let firstName = 'Camila';  /*para criarmos variáveis usamos let neste caso, lembrando que é case sensitive*/
+let lastName = 'Jesus Fontes'; /*Lembrando que no novo JS o ponto e vírgula não é necessário*/
+let age = 29;
+
+console.log('Olá, meu nome é ' + firstName + ' ' + lastName + '\nEu tenho ' + age + ' de idade');
